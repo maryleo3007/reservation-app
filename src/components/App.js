5720de3901ef3e40ead9f8cd0075510a06289758
AlejandraCP <alejandracp710@gmail.com>
2018-12-07 15:23:28 -0500

    'add-react-and-react-bootstrap'

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <p className='col-2'>recepción</p>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
