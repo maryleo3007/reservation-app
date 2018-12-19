220ea4d10fe31d594aa58d2234cfeaf4f8861db7
AlejandraCP <alejandracp710@gmail.com>
2018-12-19 13:12:19 -0500

    'refac'

--- src/components/reception/roomContainer/formsContainer.js/RoomForm.js
+++ src/components/reception/roomContainer/formsContainer.js/RoomForm.js
@@ -1,39 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+class RoomForm extends Component {
 
-const RoomForm = props =>  {   
+    state = { 
+        roomState : {
+            title: this.props.room.title,
+            state: this.props.room.state,
+            time: this.props.room.time,
+            executive: this.props.room.executive,
+            key: this.props.room.key,
+        },  
+        available: 'disponible',
+        probable: 'probable',
+        unavailable: 'ocupado'
+    }
 
-        const {title, state, time, executive, key} = props.room ;
-        const available = 'disponible';
-        const probable = 'probable';
-        const unavailable = 'ocupado'
+    changeAvailable = (e) => {
+        e.preventDefault();
+        this.props.changeState(this.state.roomState.key, this.state.available)            
+    }
 
+    changeProbable = (e) => {
+        e.preventDefault();
+        this.props.changeState(this.state.roomState.key, this.state.probable)            
+    }
 
-        let changeAvailable = (e) => {
-            e.preventDefault();
-            props.changeState(key, available)            
-        }
-
-        let changeProbable = (e) => {
-            e.preventDefault();
-            props.changeState(key, probable)            
-        }
-
-        let changeUnavailable = (e) => {
-            e.preventDefault();
-            props.changeState(key, unavailable)            
-        }
-
-        
-        return (
-            <div className="">
-            <span><b>Formulario :</b> {title}</span>
-            <button onClick={changeAvailable}>Disponible</button>
-            <button onClick={changeProbable}>Probable</button>
-            <button onClick={changeUnavailable}>Ocupado</button>
-            <button>Hora</button>
-            <button>Ejecutivo</button> 
+    changeUnavailable = (e) => {
+        e.preventDefault();
+        this.props.changeState(this.state.roomState.key, this.state.unavailable)            
+    }
+    render() { 
+        return ( 
+            <div>
+                <div>
+                    <span><b>Formulario :</b> {this.state.roomState.title} </span>
+                    <button onClick={this.changeAvailable}>Disponible</button>
+                    <button onClick={this.changeProbable}>Probable</button>
+                    <button onClick={this.changeUnavailable}>Ocupado</button>
+                    <button>Hora</button>
+                    <button>Ejecutivo</button> 
+                </div>
+                <form>
+                    <div className="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    </div>
+                    <div className="form-group form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
             </div>
-        );
+         );
+    }
 }
  
 export default RoomForm;
