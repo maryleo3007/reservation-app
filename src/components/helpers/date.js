aae2b3b8295408fc58e948a0b7849b231ee558cd
maryleo <jesus.antonio@mdp.com.pe>
2019-01-11 10:39:40 -0500

    add helper of dates

---
+++ src/components/helpers/date.js
@@ -0,0 +1,42 @@
+export function getDateFull() {
+    
+    let today = '';
+    let dd = '';
+    let mm = '';
+    let yyyy = '';
+
+    today = new Date();
+    dd = today.getDate();
+    mm = today.getMonth() + 1;
+    yyyy = today.getFullYear();
+
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+    today = dd + "/" + mm + "/" + yyyy;
+
+    return today;
+}
+export function getHour() {
+    let today = '';
+    let hours = '';
+    let minutes = '';
+    let seconds = '';
+    let ampm = '';
+    let strTime = '';
+
+    today = new Date();
+    hours = today.getHours();
+    minutes = today.getMinutes();
+    seconds = today.getSeconds();
+    ampm = hours >= 12 ? 'p.m' : 'a.m';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    seconds = seconds < 10 ? '0'+seconds : seconds;
+    strTime = hours + ':' + minutes + ':'+seconds +' ' + ampm;
+    return strTime;
+}
